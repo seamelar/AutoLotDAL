acc25b78fa3fd0ba532524e958abb7eae6e2f028
seamelar <langemelar@gmail.com>
2023-01-13 00:47:45 +0300

    Add project files.

---
+++ AutoLotDAL/AutoLotConnDAL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AutoLotConnectedLayer
+{
+    public class InventoryDAL
+    {
+        private SqlConnection connect = null;
+
+        public void OpenConnection(string connectionString)
+        {
+            connect = new SqlConnection(connectionString);
+            connect.Open();
+        }
+
+        public void CloseConnection()
+        {
+            connect.Close();
+        }
+    }
+}
